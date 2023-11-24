9848d41a38f87ce398bc1285fa67c4f130d26407
codevarty <hisu7789@g.yju.ac.kr>
2023-11-24 19:32:25 +0900

    upload Modify 3Sum.py

--- 2023-11-22/3Sum.py
+++ 2023-11-22/3Sum.py
@@ -9,11 +9,11 @@ class Solution:
         lst = []
         nums.sort()
         for i in range(len(nums) - 2):
-            if i > 0 and nums[i] == nums[i-1]:
+            if i > 0 and nums[i] == nums[i - 1]:
                 continue
 
             left = i + 1
-            right = len(nums) -1
+            right = len(nums) - 1
             while left < right:
                 sum = nums[left] + nums[right] + nums[i]
 
@@ -22,18 +22,18 @@ class Solution:
                 elif sum > 0:
                     right -= 1
                 else:
-                    lst.append([nums[left], nums[right], nums[i]])
-                    while left < right and nums[left] == nums[left+1]:
-                        left +=1
-                    while left < right and nums[right] == nums[right -1]:
-                        left -=1
-                    left +=1
-                    right-=1
+                    lst.append([nums[i], nums[left], nums[right]])
+                    while left < right and nums[left] == nums[left + 1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right - 1]:
+                        right -= 1
+                    left += 1
+                    right -= 1
         return lst
 
 
 if __name__ == '__main__':
     sol = Solution()
-    input_nums = [-1, 0, 1, 2, -1, -4]
+    input_nums = [-2, 0, 0, 2, 2]
     output = sol.threeSum(nums=input_nums)
     print(output)
